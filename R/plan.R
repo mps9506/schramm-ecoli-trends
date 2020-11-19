abe39822b07c739120973030f7c78558c3b83bfa
mps9506 <mpschramm@gmail.com>
2020-11-19 08:04:30 -0600

    density plots

--- R/plan.R
+++ R/plan.R
@@ -19,7 +19,8 @@ plan <- drake_plan(
   #gam_power = fit_power_gam(ecoli_data),
   
   ## describe functional relationships
-  dens_plot_mk_power = plot_mk_power(file_in("data/mk_power_dat"),
+  dens_plot_mk_power = plot_mk_power(site_info,
+                                     file_in("data/mk_power_dat"),
                                      file_name = file_out("figures/fig_3.png"),
                                      width = 140,
                                      height = 95,
